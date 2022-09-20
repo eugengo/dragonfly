0d065e857ea817c743faaddffb7c62bb30ad9c0d
Eugen Trippel <eu@skyworld.dev>
2022-09-20 22:24:41 +0200

    First steps

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+)
 
+func main() {
+	// Configuration
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Config error: %s", err)
+	}
+
+	// Run
+	app.Run(cfg)
 }
