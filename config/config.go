ac564bd9b72654f7edfe9c6f5abe4d29dce84322
Eugen Trippel <eu@skyworld.dev>
2022-10-01 15:57:41 +0200

    Make up config.go

--- config/config.go
+++ config/config.go
@@ -6,6 +6,38 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
+type (
+	// Config
+	Config struct {
+		App  `yaml:"app"`
+		HTTP `yaml:"http"`
+		Log  `yaml:"logger"`
+		PG   `yaml:"postgres"`
+	}
+
+	// App
+	App struct {
+		Name    string `env-required:"true" yaml:"name"		env:"APP_NAME"`
+		Version string `env-required:"true" yaml:"version"	env:"APP_VERSION"`
+	}
+
+	// HTTP
+	HTTP struct {
+		Port string `env-required:"true" yaml:"port" env:"HTTP_PORT"`
+	}
+
+	// Log
+	Log struct {
+		Level string `env-required:"true" yaml:"log_level" env:"LOG_LEVEL"`
+	}
+
+	// PG
+	PG struct {
+		PoolMax int    `env-required:"true" yaml:"pool_max" env:"PG_POOL_MAX"`
+		URL     string `env-required:"true"				 env:"PG_URL"`
+	}
+)
+
 // NewConfig returns app config.
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
